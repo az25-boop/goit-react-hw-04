156a607757b11eeb004b737fd51a3b658a98bf30
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-09-18 07:25:55 +0300

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-// import React from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "modern-normalize";
 import App from "./components/App/App.jsx";
 import "./index.css";
